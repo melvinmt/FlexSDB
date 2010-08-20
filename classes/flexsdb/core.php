3e2dca35f3641d1174f8595e53c8a0d4ea508e49
Melvin Tercan <mmtercan@gmail.com>
2010-08-21 00:39:43 +0200

    last commit for today

--- classes/flexsdb/core.php
+++ classes/flexsdb/core.php
@@ -2,6 +2,28 @@
 
 abstract class FlexSDB_Core{
 	
+	public static $longtext;
 	
+	public static function bytesConvert($bytes){
+	    $ext = array('B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB');
+	    $unitCount = 0;
+	    for(; $bytes > 1024; $unitCount++) $bytes /= 1024;
+	    return round($bytes, 2) ." ". $ext[$unitCount];
+	}
+	
+	public static function decode_val(SimpleXMLElement $value){
+		
+		$value = strval($value);
+		
+		if(is_numeric($value)){
+			
+			return (double) $value;
+
+		}else{
+			
+			return $value;
+		}
+		
+	}
 	
 }
--- classes/flexsdb/response.php
+++ classes/flexsdb/response.php
@@ -2,21 +2,35 @@
 
 class FlexSDB_Response {
 	
+	public $status = 0;
+	public $success = false;
+	public $date = 0;
+	public $total_time = 0;
+	public $size = '0 B';
+	public $single = true;
+	public $NextToken;
+	public $has_next = false;
+	public $is_empty = true;
+	public $RequestId;
+	public $BoxUsage;
+	public $error_msg;
+	public $error_code;
+	public $identifier;
 	public $contents = array();
 		
 	public function __construct(ResponseCore $obj, $single = true){
 		
+		$this->identifier = text::random('alnum', 32);
 		$this->status = $obj->status;
 		$this->date = strtotime($obj->header['date']);
 		$this->total_time = $obj->header['_info']['total_time'];
-		$this->size = $obj->header['_info']['size_download'];
+		$this->size = FlexSDB::bytesConvert($obj->header['_info']['size_download']);
 		$this->single = $single;
-		$this->success = false;
 		
 		if($this->status === 200){
 			
 			$content = $obj->body->SelectResult->Item;
-			$this->NextToken = isset($this->SelectResult->NextToken) ? $this->SelectResult->NextToken : NULL;
+			$this->NextToken = isset($obj->SelectResult->NextToken) ? $obj->SelectResult->NextToken : NULL;
 			$this->has_next = $this->NextToken !== NULL ? true : false; 
 			
 			echo Kohana::debug(count($content));
@@ -56,6 +70,9 @@ class FlexSDB_Response {
 			$this->error_code = (string) $obj->body->Errors->Error->Code;
 			$this->error_msg = (string) $obj->body->Errors->Error->Message;
 			
+			$this->RequestId = (string) $obj->body->RequestID;
+			$this->BoxUsage = (double) $obj->body->Errors->Error->BoxUsage;
+			
 		}
 		
 		
@@ -85,30 +102,28 @@ class FlexSDB_Response {
 	public function add_attribute($itemName, SimpleXMLElement $attribute){
 		
 		$key = (string) $attribute->Name;
-		$value = FlexSDB_Response::decode($attribute->Value);
+		$value = FlexSDB::decode_val($attribute->Value, $key, $this->identifier);
 		
-		if(!isset($contents[$itemName][$key])){
+		if(!isset($this->contents[$itemName][$key])){
 			
 			$this->contents[$itemName][$key] = $value;
 			
 		}else{
 			
-			if(is_array($contents[$itemName][$key])){
+			if(is_array($this->contents[$itemName][$key])){
 				
 				$this->contents[$itemName][$key][] = $value;
 				
 			}else{
 				
-				$pre_value = $contents[$itemName][$key];
+				$pre_value = $this->contents[$itemName][$key];
 				$this->contents[$itemName][$key] = array($pre_value, $value);
 			}
 		}
 		
 	}
 	
-	public static function decode($value){
+
 	
-		return (string) $value;
-		
-	}
+
 }
